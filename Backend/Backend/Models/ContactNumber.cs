7e40fb611b12ca1441e1080bb83602894d1472f5
svrsaljko <vrsaljko tjepan@gmail.com>
2020-06-10 19:36:37 +0200

    Add EntityFramework package, Add Contact,ContactEmail,ContactNumber models

---
+++ Backend/Backend/Models/ContactNumber.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Backend.Models
+{
+    public class ContactNumber
+    {
+        public int ContactNumberId { get; set; }
+        public string Number { get; set; }
+
+        public virtual Contact Contact { get; set; }
+    }
+}
